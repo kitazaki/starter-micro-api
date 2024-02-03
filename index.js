a5d41018ef8dabb6dd32673aa0e4a412e234b77d
Ayachika Kitazaki <kitazaki@gmail.com>
2024-02-04 08:56:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
+    console.log(process.env)
     res.write('Yo!');
+    res.write(process.env);
     res.end();
 }).listen(process.env.PORT || 3000);
