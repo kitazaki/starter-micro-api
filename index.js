4429f02e023b0a95a307139b60e8dc3991755b36
Ayachika Kitazaki <kitazaki@gmail.com>
2024-02-04 09:09:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     console.log(process.env)
 //    res.write('Yo!');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(process.env));
     res.end();
 }).listen(process.env.PORT || 3000);
